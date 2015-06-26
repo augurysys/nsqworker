9a9c4f8189ce9d19d2d6c2481e26044adac02bf7
Or Rikon <orikon@augury.com>
2015-06-26 12:26:10 +0300

    Allow manual registering using register_route

--- nsqworker/example.py
+++ nsqworker/example.py
@@ -3,11 +3,15 @@ from basic_matchers import json_matcher, regex_matcher
 import json
 import time
 
+# ---------------
+# -- Example 1 --
+# ---------------
+# Basic handler functionality requires subclassing NSQHandler
+# Calling load_routes decorator on it
+# Calling route(matcher_func, handler_func) on every handler
 
 @load_routes
-class DecoratorJSONPingPong(NSQHandler):
-    routes = []
-
+class JSONPingPong(NSQHandler):
     @route(json_matcher("name", "ping"))
     def ping(self, message):
         self.logger.info("Ping")
@@ -20,10 +24,13 @@ class DecoratorJSONPingPong(NSQHandler):
         time.sleep(1)
         self.send_message("test", json.dumps(dict(name="ping")))
 
-@load_routes
-class DecoratorTextPingPong(NSQHandler):
-    routes = []
+# ---------------
+# -- Example 2 --
+# ---------------
+# Notice you can register multiple routes for one handler
 
+@load_routes
+class TextPingPong(NSQHandler):
     @route(regex_matcher("^plop$"))
     @route(regex_matcher("^ping$"))
     def ping(self, message):
@@ -37,15 +44,39 @@ class DecoratorTextPingPong(NSQHandler):
         time.sleep(1)
         self.send_message("test2", "ping")
 
+# ---------------
+# -- Example 3 --
+# ---------------
+# It's also possible to register routes using the class method register_route
+# instead of the route decorator, just remember to call super for NSQHandler
+
+@load_routes
+class ThirdPingPong(NSQHandler):
+    def __init__(self, topic, channel):
+        super(ThirdPingPong, self).__init__(topic, channel)
+        self.register_route(regex_matcher("^ping$"), self.ping)
+        self.register_route(regex_matcher("^pong$"), self.pong)
+
+    def ping(self, message):
+        self.logger.info("Ping")
+        time.sleep(1)
+        self.send_message("test3", "pong")
+
+    def pong(self, message):
+        self.logger.info("Pong")
+        time.sleep(1)
+        self.send_message("test3", "ping")
 
 import nsq
 
-DecoratorJSONPingPong("test", "pingpong")
-DecoratorTextPingPong("test2", "pingpong2")
+JSONPingPong("test", "pingpong")
+TextPingPong("test2", "pingpong2")
+ThirdPingPong("test3", "pingpong3")
 
 nsq.run()
 
 
+# TODO - take get_logger outside of functions, maybe make it into it's own package
 # TODO - Add priorities
 # TODO - loglevel setting of nsqhandler (to be like ThreadWorker)
 # TODO - message encryption
