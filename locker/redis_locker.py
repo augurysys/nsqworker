9cb45880a8ea321e70f02e0d5b1310ab7f31946f
ovaltzer <ovaltzer@augury.com>
2016-12-01 11:04:44 +0200

    SOFTWARE-946 : add sleep between retries, comments and key getter

--- locker/redis_locker.py
+++ locker/redis_locker.py
@@ -1,6 +1,7 @@
 import os
 
 import redis as redis_client
+import time
 
 REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PORT = os.environ.get("REDIS_PORT")
@@ -21,31 +22,67 @@ class RedisLocker:
         self.service_name = service_name
 
     def get_lock_object(self, key, lock_options):
-        return RedisLock(key, lock_options, self.redis)
+        return RedisLock(key, lock_options, self.service_name, self.redis)
 
 
 class RedisLock:
-    def __init__(self, key, lock_options, redis):
-        self.__lock_obj = redis.lock(name=key, timeout=lock_options.ttl, blocking_timeout=lock_options.timeout,
+    """
+    This class represent a redis lock object. A client who would like to lock a resource, should create this object
+    and hold it. There are 2 main methods: lock, unlock
+    """
+
+    def __init__(self, key, lock_options, service_name, redis):
+        self.__lock_obj = redis.lock(name=self.get_key(service_name, key), timeout=lock_options.ttl, blocking_timeout=lock_options.timeout,
                                      sleep=LOCKED_RETRY_DURATION)
+        self.__retries = lock_options.retries
 
     def lock(self):
+        """
+        This method tries to acquire a lock. If a redis error is raised during the process, it will try again for
+        ``self.__lock_obj.retries`` times
+        :return: True if the resource is locked, False o.w
+        :raise: ``RedisError`` in case of redis returned error while trying to lock and all retries were used
+        """
         err = None
-        for i in xrange(0,self.__lock_obj.retries):
+        for i in xrange(0, self.__retries):
             try:
                 return self.__lock_obj.acquire()
             except redis_client.RedisError as re:
                 err = re
+                if i != self.__lock_obj.retries - 1:
+                    time.sleep(ERR_RETRY_DURATION)
                 continue
         raise err
 
-
     def unlock(self):
-        return self.__lock_obj.release()
+        """
+        unlocks the lock object
+        :return: there is no return for this function, if everything goes well, no exception will be thrown
+        :raise: ``RedisError`` - general error with redis, there are 2 special cases where there will be raised
+        ``LockError(RedisError)`` if the lock does not exist or if the lock is owned by a different owner (the lock
+        token is different).
+        """
+        self.__lock_obj.release()
+
+    @staticmethod
+    def get_key(service_name, resource):
+        resource = "".join(resource.split())  # remove all white spaces
+        return "{}:lock:{}".format(service_name, resource)
 
 
 class LockOptions:
-    def __init__(self):
-        self.ttl = DEFAULT_TTL
-        self.timeout = DEFAULT_TIMEOUT
-        self.retries = DEFAULT_RETRIES
+    """
+    Lock Object, should be used by locking mechanism clients for defining the lock object
+    """
+
+    def __init__(self, ttl=DEFAULT_TTL, timeout=DEFAULT_TIMEOUT, retries=DEFAULT_RETRIES):
+        """
+        Create a new lock object
+        ``ttl`` - the expiry time of the lock
+        ``timeout`` - the amount of time which the locker will try to get a lock while the resource is locked by
+                    another process
+        ``retries`` - the amount of retries which in case of redis error the locker will try to lock
+        """
+        self.ttl = ttl
+        self.timeout = timeout
+        self.retries = retries
