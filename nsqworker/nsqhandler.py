40e2326b4d77480f09ae99f278434ae1da10236c
Assaf Mizrachi <amizrachi@augury.com>
2017-07-26 09:49:51 +0300

    test

--- nsqworker/nsqhandler.py
+++ nsqworker/nsqhandler.py
@@ -242,19 +242,20 @@ class NSQHandler(NSQWriter):
 
             except Exception as e:
                 if is_idempotent:
-                    retry_count = jsn['retry_count'] + 1 if "retry_count" in jsn else 1
-                    if retry_count <= RETRY_LIMIT:
-                        # in case we did not reach the retry limit, we send a new message with the specific failed
-                        # recipient and increasing the retry counter. We cannot use nsq's requeue mechanism because
-                        # we must send the event to the specific handler so we wont run all related handlers again
-                        # (which are not guaranteed to be idempotent as well)
-                        self._construct_recovery_message(jsn, handler.__name__, retry_count)
-                        NSQHandler.send_message(self, self.topic, json.dumps(jsn),
-                                                delay=RETRY_DELAY_DURATION * retry_count)
-                        self.logger.info("[{}] [END] [route={}] [event={}] [status={}] [retry_count={}] [time={}] "
-                                         .format(route_id, handler.__name__, event_name, "RESENT", retry_count,
-                                                 str(current_milli_time() - start_time)))
-                        continue
+                    message.requeue()
+                    # retry_count = jsn['retry_count'] + 1 if "retry_count" in jsn else 1
+                    # if retry_count <= RETRY_LIMIT:
+                    #     # in case we did not reach the retry limit, we send a new message with the specific failed
+                    #     # recipient and increasing the retry counter. We cannot use nsq's requeue mechanism because
+                    #     # we must send the event to the specific handler so we wont run all related handlers again
+                    #     # (which are not guaranteed to be idempotent as well)
+                    #     self._construct_recovery_message(jsn, handler.__name__, retry_count)
+                    #     NSQHandler.send_message(self, self.topic, json.dumps(jsn),
+                    #                             delay=RETRY_DELAY_DURATION * retry_count)
+                    #     self.logger.info("[{}] [END] [route={}] [event={}] [status={}] [retry_count={}] [time={}] "
+                    #                      .format(route_id, handler.__name__, event_name, "RESENT", retry_count,
+                    #                              str(current_milli_time() - start_time)))
+                    continue
 
                 status = "FAILED"
                 msg = "[{}] Handler {} failed handling message {} with error {}".format(
