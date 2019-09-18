87d94c6f1b6192f8a4a57b7ede28fe247c4e45b9
guycarmy <guycarmy@gmail.com>
2019-09-18 14:07:26 +0300

    Use new auguryapi

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['nsqworker', 'locker'],
     version='0.0.1',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures', 'mdict', 'redis',
-    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@7e29797ff6abb30f372ce4950a1e53cca05da85e#egg=auguryapi'
+    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@1d5537c7a454e6ca96424a54effddbe022f6a09b#egg=auguryapi'
     ],
 )
