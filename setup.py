9f3dbb1d763f733d2e4c72e0e51b429022517683
guycarmy <guycarmy@gmail.com>
2019-09-18 17:49:21 +0300

    Update opencensus dependency

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['nsqworker', 'locker'],
     version='0.0.1',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures', 'mdict', 'redis',
-    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@c8e5ede91fb6fbe31f38170774379255a86a79fb#egg=auguryapi'
+    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@395b1ce0269d9805406660e44c11c37e6fc9dee1#egg=auguryapi'
     ],
 )
