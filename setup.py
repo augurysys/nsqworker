ea30defbb6fbfb73db671e67e4e4e1bb66046267
guycarmy <guycarmy@gmail.com>
2019-09-18 13:31:43 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
 
+
+with open('requirements.txt', 'rt') as f_req:
+    rows = f_req.readlines()
+    reqs = []
+    dep_links = []
+    for req in rows:
+        if not req.startswith('git+'):
+            reqs.append(req)
+        else:
+            dep_links.append('"{}"'.format(req))
+
 setup(
     name='nsqworker',
     packages=['nsqworker', 'locker'],
     version='0.0.1',
-    install_requires=['tornado==4.5.3', 'pynsq', 'futures', 'mdict', 'redis'],
+    install_requires=reqs,
+    dependency_links=dep_links,
 )
