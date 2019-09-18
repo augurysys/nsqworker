c166da45869e12905b3e176a1e52b1eab74cf793
guycarmy <guycarmy@gmail.com>
2019-09-18 15:20:21 +0300

    Update dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['nsqworker', 'locker'],
     version='0.0.1',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures', 'mdict', 'redis',
-    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@1d5537c7a454e6ca96424a54effddbe022f6a09b#egg=auguryapi'
+    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@c8e5ede91fb6fbe31f38170774379255a86a79fb#egg=auguryapi'
     ],
 )
