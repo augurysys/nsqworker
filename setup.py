3841c3e1c296cbfb9257ed4369e5ef9d22c957c3
guycarmy <guycarmy@gmail.com>
2019-09-24 14:42:53 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='nsqworker',
     packages=['nsqworker', 'locker'],
-    version='0.0.1',
+    version='0.0.2',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures', 'mdict', 'redis',
-    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@395b1ce0269d9805406660e44c11c37e6fc9dee1#egg=auguryapi'
+    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@0.9.33#egg=auguryapi'
     ],
 )
