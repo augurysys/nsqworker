d6cc0d26f6488f99fd40def30a34c4993091f244
Idan Mann <imann@augury.com>
2021-02-11 17:57:59 +0200

    Latest augurypi-py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['nsqworker', 'locker'],
     version='0.0.2',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures', 'mdict', 'redis',
-    'git+ssh://git@github.com/augurysys/auguryapi-py.git@577f2cbdecca0bae3419252497d1d38070416a7d#egg=auguryapi'
+    'auguryapi @git+ssh://git@github.com/augurysys/auguryapi-py.git@0.9.34#egg=auguryapi'
     ],
 )
