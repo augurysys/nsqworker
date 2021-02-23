6e27bca4910a452583e48513b4f332e19af68b60
Idan <32977650+IdanMann@users.noreply.github.com>
2021-02-23 12:56:54 +0200

    Update setup.py
    Bumped package version to match the repo tag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='nsqworker',
     packages=['nsqworker', 'locker'],
-    version='0.0.3',
+    version='0.0.7',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures', 'mdict', 'redis',
     'auguryapi @git+https://github.com/augurysys/auguryapi-py.git@0.9.34#egg=auguryapi'
     ],
