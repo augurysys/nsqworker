5715e69db70737f4130cc0537190a5a056469152
Tomer <tomer.g2@gmail.com>
2021-04-12 23:51:42 +0300

    migrated functions from python microservice to nsqworker

--- nsqworker/helpers.py
+++ nsqworker/helpers.py
@@ -46,3 +46,71 @@ def post_topic(nsq_http, topic):
             )
         )
         return False
+
+
+def _discover_nsqd(nsq_topic, lookupd_http_addresses=None):
+    topic_exists = False
+    nsqd_nodes_list = list()
+    environment_nsqd_tcp_addresses = os.getenv("NSQD_TCP_ADDRESSES")
+    try:
+        environment_nsqd_tcp_addresses = environment_nsqd_tcp_addresses.split(",")
+    except AttributeError:
+        environment_nsqd_tcp_addresses = environment_nsqd_tcp_addresses
+    if not lookupd_http_addresses:
+        environment_lookupd = os.getenv("LOOKUPD_HTTP_ADDRESSES")
+        lookupd_http_addresses = environment_lookupd if lookupd_http_addresses else []
+    if isinstance(lookupd_http_addresses, str):
+        lookupds_list = lookupd_http_addresses.split(",")
+    elif isinstance(lookupd_http_addresses, list):
+        lookupds_list = lookupd_http_addresses
+    else:
+        lookupds_list = []
+
+    for lookup_endpoint in lookupds_list:
+        producers_result = requests.get(f"http://{lookup_endpoint}/nodes")
+        producers_list = producers_result.json().get('producers')
+        for producer_dict in producers_list:
+            produced_topics_list = producer_dict.get("topics")
+            if nsq_topic in produced_topics_list:
+                nsqd_address = producer_dict.get("broadcast_address")
+                nsqd_tcp_port = producer_dict.get("tcp_port")
+                nsqd_host = f"{nsqd_address}:{nsqd_tcp_port}"
+                nsqd_nodes_list.append(nsqd_host)
+    if len(lookupds_list) == 0:
+        external_logger.info(f"Found no nsqd that hold the topic [{nsq_topic}], "
+                             f"defaulting to {environment_nsqd_tcp_addresses}")
+        return environment_nsqd_tcp_addresses, topic_exists
+    else:
+        topic_exists = True
+        return nsqd_nodes_list, topic_exists
+
+
+def random_nsqd_selector(nsq_topic, lookupd_http_addresses=None):
+    nsqd_nodes_list, topic_exists = _discover_nsqd(nsq_topic=nsq_topic,
+                                                  lookupd_http_addresses=lookupd_http_addresses)
+    external_logger.info(f"nsq nodes list is: {nsqd_nodes_list}")
+    nsqd_tcp = random.choice(nsqd_nodes_list)
+    external_logger.info(f"Will now use: {nsqd_tcp}")
+    nsqd_http = nsqd_tcp.replace("4150", "4151")
+    if not topic_exists:
+        external_logger.info(f"Topic [{nsq_topic}] doesn't exist, creating one.")
+        post_message_to_nsq(nsqd_http_address=nsqd_http,
+                            topic=nsq_topic,
+                            message_payload="Mocked payload")
+    return nsqd_tcp, nsqd_http
+
+
+def _post_using_requests(url, data):
+    return requests.post(url=url, data=data)
+
+
+def post_message_to_nsq(nsqd_http_address, topic, message_payload):
+    # Build post url
+    post_url = f"http://{nsqd_http_address}/pub?topic={topic}"
+    try:
+        post_result = _post_using_requests(url=post_url, data=message_payload)
+        external_logger.info(f"Published message: {message_payload}"[:100])
+        return post_result
+    except Exception as e:
+        external_logger.error(f"Failed to post result to {post_url}, Exception: {e}")
+        return None
