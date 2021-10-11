6febb2ca477499189be96618840b818bac0b8a8f
tgoldfeder <tgoldfeder@augury.com>
2021-10-11 11:56:26 +0300

    remove future package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='nsqworker',
     packages=['nsqworker', 'locker'],
     version='0.0.11',
-    install_requires=['tornado==4.5.3', 'pynsq', 'futures', 'mdict', 'redis',
+    install_requires=['tornado==4.5.3', 'pynsq', 'mdict', 'redis',
     'auguryapi @git+https://github.com/augurysys/auguryapi-py.git@0.9.34#egg=auguryapi'
     ],
 )
