6160895ba3c554784458af28361eb7ec6d5f33c4
Dmitry Polyanovsky <dmitry.polyanovsky@gmail.com>
2022-07-01 09:21:12 +0300

    py3 compatibility

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['nsqworker', 'locker'],
     version='0.0.12',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures; python_version == "2.7"', 'mdict', 'redis',
-                      'auguryapi @git+https://github.com/augurysys/auguryapi-py.git#egg=auguryapi'
+                      'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git'
                       ],
 )
