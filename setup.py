833aa489d6ac2b4a477746b6c12fd0bf055371d8
Dmitry Polyanovsky <dmitry.polyanovsky@gmail.com>
2022-07-01 09:22:51 +0300

    py3 compatibility

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='nsqworker',
     packages=['nsqworker', 'locker'],
-    version='0.0.12',
+    version='0.0.13',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures; python_version == "2.7"', 'mdict', 'redis',
                       'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git'
                       ],
