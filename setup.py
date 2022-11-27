4aa55eb38afc60468caabc2333da99de3582330a
BenBizinsky <bbizinsky@augury.com>
2022-11-28 00:03:35 +0200

    change ver

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version='0.0.13',
     install_requires=['tornado==5.1.1', 'pynsq', 'futures; python_version == "2.7"', 'mdict', 'redis',
                       'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git'
-                      '@ab7b15ec849c33c87ab4ebfa7e8bee67350bda69'
+                      '@b4385c71e3be38f3731acfc3d9d5c71c5044d432'
                       ],
 )
