8b1236bf1a7e1e912a9c6abd0386088d7ea362c4
sdabush <sdabush@augury.com>
2022-11-27 14:37:11 +0200

    update auguryapi vesrion for the tests

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     version='0.0.13',
     install_requires=['tornado==5.1.1', 'pynsq', 'futures; python_version == "2.7"', 'mdict', 'redis',
                       'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git'
+                      '@8c08a5267c7275d31c541c7986ab52f07dc841c2 '
                       ],
 )
