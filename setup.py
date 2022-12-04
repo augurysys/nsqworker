c15cfeb165f97e018c9af5e6517928e4c0f2f7ff
BenBizinsky <bbizinsky@augury.com>
2022-12-04 13:09:19 +0200

    bump

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version='0.0.13',
     install_requires=['tornado==5.1.1', 'pynsq', 'futures; python_version == "2.7"', 'mdict', 'redis',
                       'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git'
-                      '@e3cc09139a814d3253a6af9555d8b06fa7dcda5a'
+                      '@88670b6988e77f4f4812b991e28ae51502ddcd0e'
                       ],
 )
