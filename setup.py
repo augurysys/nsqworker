9a4b44a885f05bdec994311c72cc6cfd8c913991
BenBizinsky <bbizinsky@augury.com>
2022-12-18 14:44:53 +0200

    test

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     packages=['nsqworker', 'locker'],
     version='0.0.13',
     install_requires=['tornado==5.1.1', 'pynsq', 'futures; python_version == "2.7"', 'mdict', 'redis',
-                      'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git@0.9.39'],
+                      'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git@8b046e60acc8ff3ec711102d5dcd23c0b0ee950e'],
 )
