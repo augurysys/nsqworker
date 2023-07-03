787811e8563a5159b2b183ffb4b03edaae3d3aa0
bsolomon <bsolomon@augury.com>
2023-07-03 16:34:38 +0300

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='nsqworker',
     packages=['nsqworker', 'locker'],
-    version='0.0.18',
+    version='0.0.19',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures; python_version == "2.7"', 'mdict', 'redis',
-                      'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git@0.9.40'],
+                      'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git@0.9.48'],
 )
