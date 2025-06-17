965812522d42a8c933e791a01a04cb4db88264be
rchaudhary-augury <rchaudhary@augury.com>
2025-06-17 10:03:34 +0530

    bumped auguryapi-py to latest version for requirement

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     packages=['nsqworker', 'locker'],
     version='0.0.26',
     install_requires=['tornado==4.5.3', 'pynsq', 'futures; python_version == "2.7"', 'mdict', 'redis',
-                      'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git@0.9.63'],
+                      'auguryapi @ git+https://github.com/augurysys/auguryapi-py.git@0.9.64'],
 )
